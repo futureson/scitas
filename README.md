da0762777109a87a58766a498fe20a49b80c4944
futureson <qiming.sun@epfl.ch>
2024-07-29 04:41:33 -0700

    working on scitas

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# scitas
+# Scitas
 Scripts or tips for running on EPFL SCITAS 
+
+## 1. Introduction to the SCITAS cluster
+
+The SCITAS cluster is a high-performance computing (HPC) cluster at EPFL. It is a shared resource that is available to all EPFL researchers. The cluster is composed of several nodes, each with multiple cores and a large amount of memory. The cluster is managed by the SCITAS team, who are responsible for maintaining the hardware and software, as well as providing support to users. More information about the SCITAS cluster can be found on the [SCITAS website](https://www.epfl.ch/research/facilities/scitas/) and in the [SCITAS user guide](https://scitas-doc.epfl.ch/).
+
+For our use and this repository, we will focus on the following aspects of the SCITAS cluster:
+- ***Jed*** cluster (***@jed.epfl.ch***) is the one we used
+- 2 directories are used 
+    - `/scratch/{user}/` for temporary storage, no storage limit, but files are deleted after [15/30](https://scitas-doc.epfl.ch/storage/file-system/#occupancy-limits) days. 
+        - All files after 30 days will be deleted
+        - If with high occupancy, files may be deleted after 2 weeks. but files within 2 weeks seems to be safe
+    - `/home/{user}` for login in and running scripts, 100GB limit, data will be erased after two years of inactivity or 6 months after leaving EPFL, whichever occurs first.
+
+## 2. Useful commands
+
+### 2.1. Terminal Setup
+
+It is recommended to use the Ubuntu terminal to connect to the SCITAS cluster. Following the [instructions on Windows](https://learn.microsoft.com/en-us/windows/wsl/install) to install the WSL linux terminal. Or we can also use the ***Git Bash*** terminal (Powershell terminal could also work for logging in, but the command `rsync` doesn't work, you should use `scp` instead, but it would be much slower).
+
+To install `rsync` for Git Bash terminal, see [here](https://prasaz.medium.com/add-rsync-to-windows-git-bash-f42736bae1b3) for more details.
+
+### 2.2. Useful commands and links
+
+- **Login to the SCITAS cluster**: 
+
+    [https://scitas-doc.epfl.ch/user-guide/using-clusters/connecting-to-the-clusters/](https://scitas-doc.epfl.ch/user-guide/using-clusters/connecting-to-the-clusters/)  
+    ```bash
+    ssh {user}@jed.epfl.ch
+    ```
+- **Introduction to Linux commands**:
+    
+    [https://www.epfl.ch/research/facilities/scitas/documentation/training/#intro](https://www.epfl.ch/research/facilities/scitas/documentation/training/#intro)
+
+# 3. Running Jobs on the SCITAS cluster with python
+Here we will show how to run jobs on the SCITAS cluster using python. We will use the `paramiko` library to connect to the cluster and run commands.
+
+
+
+
+
+
---
+++ Sim_Jul_2024/func.py
@@ -0,0 +1,118 @@
+import os
+import time
+import shutil
+import paramiko
+
+def generate_slurm_script(user, crop, gim, scenario):
+    slurm_script = f"""#!/bin/sh
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --time 72:00:00
+#SBATCH --mem=80G
+#SBATCH -e slurm-%j.err
+#SBATCH -o slurm-%j.out
+#SBATCH --mail-type=end
+#SBATCH --mail-user=qiming.sun@epfl.ch
+RUN=/scratch/{user}
+cd $RUN/CODE
+
+module load matlab/R2024a
+
+# time matlab -nodisplay -r "mainp('Wheat','CWatM','historical')"; 
+time matlab -nodisplay -r "mainp({crop!r},{gim!r},{scenario!r})"; 
+
+exit;"""
+    output_file = 'torun/' + crop[0:3] + '_' + gim[0:2] + scenario[3:5] + '.job'
+    jobfile = crop[0:3] + '_' + gim[0:2] + scenario[3:5] + '.job'
+    with open(output_file, 'w', newline= '\n') as file: # newline= '\n' is used to fit the linux format
+        file.write(slurm_script)
+    print(f"SLURM job script '{output_file}' created successfully.")
+    return jobfile
+
+def connect(user, password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname = 'jed.epfl.ch', username=user, password=password)
+    return ssh
+
+def check_job_status(ssh, user):
+    stdin, stdout, stderr = ssh.exec_command(f"squeue -u {user}")
+
+    # print the status of the jobs in good format
+    status = stdout.read().decode().split('\n')
+    # if status is not empty, meaning the jobs are running
+    if status:
+        print('Jobs are running')
+    else:
+        print('No jobs running')
+
+    return status
+
+def upload_data(ssh, user, gim, i, gim_path_prefix):
+    local_gim_path = gim_path_prefix + gim + '/'
+    remote_gim_path = f'{user}@jed.epfl.ch:/scratch/{user}/CODE/ALL_elabs_ISIMIP/OutputData/water_global/{gim}'
+    remote_dir= f'/scratch/{user}/CODE/ALL_elabs_ISIMIP/OutputData/water_global'
+    if i == 0:
+        ssh.exec_command(f'rm -r {remote_dir}')
+        ssh.exec_command(f'mkdir {remote_dir}')
+    # check if the remote directory exists
+    stdin, stdout, stderr = ssh.exec_command(f'ls {remote_gim_path.split(":")[-1]}')
+    if 'No such file or directory' in stderr.read().decode():
+        print('All files needs upload')
+        os.system(f'powershell.exe scp -r {local_gim_path} {remote_gim_path}') # upload through powershell scp needs ssh key setup otherwise it will ask for password and wait a long time
+    else:
+        print('Remote data already exists')
+
+def submit_job(ssh, user, gim, crops_torun, scenarios):
+    # every time generate the slurm script for 1 gim, 2 crops and 4 scenarios until all crops are done
+    jobs = []
+    for crop in crops_torun:
+        for scenario in scenarios:
+            jobfile = generate_slurm_script(user, crop, gim, scenario)
+            # copy the slurm script to the server
+            sftp = ssh.open_sftp()
+            sftp.put(f'torun/{jobfile}', f'/home/{user}/{jobfile}')
+            sftp.close()
+
+            # run the slurm script
+            stdin, stdout, stderr = ssh.exec_command(f'sbatch {jobfile}')
+            time.sleep(1) # wait for 1 second before checking the job status
+
+            # move the job file to running directory locally
+            shutil.move(f'torun/{jobfile}', f'running/{user}/{jobfile}')
+            jobs.append(jobfile)
+    print(f"The following jobs are running: {jobs}")
+
+# def download_results(ssh, user, local_res_path):
+
+#     stdin, stdout, stderr = ssh.exec_command(f"squeue -u {user}")
+#     status = stdout.read().decode().split('\n')
+
+#     remote_res_path = f'{user}@jed.epfl.ch:/scratch/{user}/CODE/SIM_RESULTS/*'
+#     if status[1:-1] == []:
+#         print('Jobs are Done')
+#         os.system(f'powershell.exe scp -r {remote_res_path} {local_res_path}')
+#         print('Results are downloaded')
+#         # ssh.exec_command(f"rm -r {remote_res_path}")
+#         # archive all jobs when Status is empty
+#         ssh.exec_command("mv *.job *.err *.out finished")
+#         jobs = os.listdir(f'running/{user}')
+#         for jobfile in jobs:
+#             shutil.move(f'running/{user}/{jobfile}', f'finished/{user}/{jobfile}')
+#     else:
+#         print('Jobs are Running')
+
+# def run(user, password, gim, interval = 3600, crops, scenarios, gim_path_prefix, local_res_path):
+
+#     ssh = connect(user, password)
+
+#     for i in range(0,4):
+#         crops_torun = crops[2*i:2*(i+1)]
+#         print('crops to run:', crops_torun)
+
+#         upload_data(ssh, user, gim, i, gim_path_prefix)
+        
+#         submit_job(ssh, user, gim, crops_torun, scenarios)
+
+#         check_job_status(ssh, user, interval, i, local_res_path) # check every hour and download data when finished 
